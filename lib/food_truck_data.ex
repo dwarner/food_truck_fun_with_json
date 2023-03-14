0ff6d322db173cfeee993c82787462a1d8886d60
Dennis Warner <dwarner@smartpaylease.com>
2023-03-14 07:30:42 -0400

    FoodTruckData is used to consume JSON from public API

---
+++ lib/food_truck_data.ex
@@ -0,0 +1,35 @@
+defmodule FoodTruckData do
+  @moduledoc """
+  Module to consume food truck data from public API.
+  """
+  require Logger
+
+  @source "https://data.sfgov.org/resource/rqzj-sfat.json"
+
+  @doc """
+  Gets and returns food truck data as a list of maps from `source`.
+
+  If no `source` is passed in, it will default to
+  @source
+  set in this module.
+
+  ## Examples
+
+    iex> FoodTruckData.get_food_truck_data()
+    {:ok, [%{}, ...]}
+
+    iex> FoodTruckData.get_food_truck_data("bad source")
+    {:error}
+
+  """
+  def get_food_truck_data(source \\ @source) do
+    try do
+      %HTTPoison.Response{status_code: 200, body: body} = HTTPoison.get!(source)
+      {:ok, Jason.decode!(body)}
+    rescue
+      e ->
+        Logger.error(Exception.format(:error, e, __STACKTRACE__))
+        {:error}
+    end
+  end
+end
