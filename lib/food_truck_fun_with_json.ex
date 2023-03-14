14701a1db464ed691148d76f150473235612f970
Dennis Warner <dwarner@smartpaylease.com>
2023-03-14 11:49:58 -0400

    Added functions to find nearest food truck serving a food
    Added functions:
    * find_nearest_with_food/3
    * find_nearest/3
    * find_food_trucks_serving/3

--- lib/food_truck_fun_with_json.ex
+++ lib/food_truck_fun_with_json.ex
@@ -4,15 +4,82 @@ defmodule FoodTruckFunWithJson do
   """
 
   @doc """
-  Hello world.
+  Takes a string `food_type` and lat and long to search for nearest food truck.
+
+  Returns the closest, APPROVED food truck with that food.
 
   ## Examples
 
-      iex> FoodTruckFunWithJson.hello()
-      :world
+      iex> FoodTruckFunWithJson.find_nearest_with_food("hot dogs", 37.788353479836715, -122.40682576358824)
+      %{":@computed_region_bh8s_q3mv" => "28861"}
+
+      iex> FoodTruckFunWithJson.find_nearest_with_food("hot dogs", 37.788353479836715, -122.40682576358824)
+      {:error, %{source: "http://asdfasdf"}}
+
+     iex> FoodTruckFunWithJson.find_nearest_with_food("no approved food truck with food", 37.788353479836715, -122.40682576358824)
+      ** (Enum.EmptyError) empty error
 
   """
-  def hello do
-    :world
+  def find_nearest_with_food(food_type, lat, long) do
+    with {:ok, food_truck_data} <- FoodTruckData.get_food_truck_data() do
+      food_truck_data
+      |> find_food_trucks_serving(food_type)
+      |> find_nearest(lat, long)
+    end
+  end
+
+  @doc """
+  Takes a list of food trucks in map format along with the lat and long to
+  search for the nearest food truck.
+
+  Returns the closest, APPROVED food truck to the lat and long passed in.
+
+  ## Examples
+
+      iex> FoodTruckFunWithJson.find_nearest([%{}], 37.788353479836715, -122.40682576358824)
+      %{":@computed_region_bh8s_q3mv" => "28861"}
+
+  """
+  def find_nearest(food_trucks, lat, long) do
+    Enum.min_by(food_trucks, fn x ->
+      {latitude, _} = Float.parse(x["longitude"])
+      {longitude, _} = Float.parse(x["latitude"])
+      Distance.GreatCircle.distance({latitude, longitude}, {lat, long})
+    end)
+  end
+
+  @doc """
+  Takes a list of food trucks in map format along with the `food_type`
+  and the `status` and reduces the list to those food trucks that match.
+
+  Defaults to "APPROVED" food trucks.
+
+  Returns a list of food trucks in map format.
+
+  ## Examples
+
+      iex> FoodTruckFunWithJson.find_food_trucks_serving([%{}], "hot dogs")
+      [%{":@computed_region_bh8s_q3mv" => "28861"}]
+
+  """
+  def find_food_trucks_serving(food_trucks, food_type, status \\ "APPROVED") do
+    approved_trucks_with_food_type =
+      Enum.reduce(food_trucks, [], fn e, acc ->
+        try do
+          %{
+            "status" => ^status,
+            "facilitytype" => "Truck",
+            "fooditems" => food_items,
+            "latitude" => _latitude,
+            "longitude" => _longitude
+          } = e
+
+          if String.match?(food_items, ~r{#{food_type}}i), do: [e | acc], else: acc
+        rescue
+          MatchError -> acc
+        end
+      end)
+
+    approved_trucks_with_food_type
   end
 end
--- test/food_truck_fun_with_json_test.exs
+++ test/food_truck_fun_with_json_test.exs
@@ -1,8 +1,79 @@
 defmodule FoodTruckFunWithJsonTest do
   use ExUnit.Case
-  doctest FoodTruckFunWithJson
 
-  test "greets the world" do
-    assert FoodTruckFunWithJson.hello() == :world
+  @food_trucks [
+    %{
+      "facilitytype" => "Truck",
+      "fooditems" => "Tacos: Burritos: Quesadillas: Tortas: Nachos: Hot Dogs:Soda: Water: Fruit Drinks",
+      "latitude" => "37.81019301997575",
+      "longitude" => "-122.5552219061259",
+      "status" => "APPROVED"
+    },
+    %{
+      "facilitytype" => "Truck",
+      "fooditems" => "Hard tacos",
+      "latitude" => "38.71019301997575",
+      "longitude" => "-123.4552219061259",
+      "status" => "APPROVED"
+    },
+    %{
+      "facilitytype" => "Truck",
+      "fooditems" => "Soft tacos",
+      "latitude" => "38.81019301997575",
+      "longitude" => "-123.5552219061259",
+      "status" => "APPROVED"
+    },
+    %{
+      "facilitytype" => "Cart",
+      "fooditems" => "Soft tacos",
+      "latitude" => "38.91019301997575",
+      "longitude" => "-123.6552219061259",
+      "status" => "APPROVED"
+    },
+    %{
+      "facilitytype" => "Truck",
+      "fooditems" => "Soft tacos",
+      "latitude" => "39.71019301997575",
+      "longitude" => "-124.4552219061259",
+      "status" => "EXPIRED"
+    },
+    %{
+      "facilitytype" => "Truck",
+      "fooditems" => "Pizza by the slice",
+      "latitude" => "39.91019301997575",
+      "longitude" => "-124.4552219061259",
+      "status" => "APPROVED"
+    }
+  ]
+
+  test "find_food_trucks_serving/3 only returns APPROVED by default" do
+    assert FoodTruckFunWithJson.find_food_trucks_serving(@food_trucks, "tacos") |> length == 3
+  end
+
+  test "find_food_trucks_serving/3 with status APPROVED only returns APPROVED" do
+    assert FoodTruckFunWithJson.find_food_trucks_serving(@food_trucks, "tacos", "APPROVED") |> length == 3
+  end
+
+  test "find_food_trucks_serving/3 with status EXPIRED only returns EXPIRED" do
+    assert FoodTruckFunWithJson.find_food_trucks_serving(@food_trucks, "tacos", "EXPIRED") |> length == 1
+  end
+
+  test "find_food_trucks_serving/3 only returns trucks" do
+    assert FoodTruckFunWithJson.find_food_trucks_serving(@food_trucks, "tacos") |> length == 3
+  end
+
+  test "find_food_trucks_serving/3 only returns trucks serving pizza" do
+    assert FoodTruckFunWithJson.find_food_trucks_serving(@food_trucks, "pizza") |> length == 1
+  end
+
+  test "find_nearest/3 returns the nearest food truck" do
+    nearest_foodtruck = FoodTruckFunWithJson.find_nearest(@food_trucks, 37.71019301997575, -122.4552219061259)
+    assert nearest_foodtruck == %{
+      "facilitytype" => "Truck",
+      "fooditems" => "Tacos: Burritos: Quesadillas: Tortas: Nachos: Hot Dogs:Soda: Water: Fruit Drinks",
+      "latitude" => "37.81019301997575",
+      "longitude" => "-122.5552219061259",
+      "status" => "APPROVED"
+    }
   end
 end
